1f8872c90f7c59245dcb14da50f56e64c9bbba1f
rawatsaheb5 <diamondcut2004@gmail.com>
2025-03-22 00:23:13 +0530

    added env file

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const { connectDB } = require("./config/db");
 require("dotenv").config();
 
-
+const router = require('./routes/index')
 
 const app = express();
 connectDB()
@@ -15,8 +15,8 @@ connectDB()
 app.use(express.json()); 
 app.use(cors()); 
 
-app.use("/api", );
+app.use("/api",router );
 
 // Start Server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
