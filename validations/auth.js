975004b7064737b4d255f75f9227f44f268f89f1
rawatsaheb5 <diamondcut2004@gmail.com>
2025-06-11 22:09:29 +0530

    added zod validation

---
+++ validations/auth.js
@@ -0,0 +1,22 @@
+const { z } = require("zod");
+
+const registerSchema = z.object({
+  name: z
+    .string()
+    .min(2, "Name must be at least 2 characters")
+    .max(50, "Name must be under 50 characters"),
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .max(50, "Password must be under 50 characters"),
+});
+
+const loginSchema = z.object({
+  email: z.string().email("Invalid email address"),
+  password: z
+    .string()
+    .min(6, "Password must be at least 6 characters")
+    .max(50, "Password must be under 50 characters"),
+});
+module.exports = { registerSchema, loginSchema };
