bcaa88a408414bdc221f2b4ee3f764b940835946
rawatsaheb5 <diamondcut2004@gmail.com>
2025-06-21 22:17:36 +0530

    updated expiry for cookie

--- constants/constant.js
+++ constants/constant.js
@@ -1,6 +1,6 @@
 const AccessTokenExpiry = "1h"
 const RefreshTokenExpiry = "7d";
-const AccessTokenCookieExpiry = 15*60*1000; // 15 min
+const AccessTokenCookieExpiry = 200*60*1000;
 const RefreshTokenCookieExpiry = 7 * 24 * 60 * 60 * 1000; ;
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET
