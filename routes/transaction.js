f99a2e3ec47c1942cc424ce183c3d64eee9d5a23
rawatsaheb5 <diamondcut2004@gmail.com>
2025-06-21 22:18:05 +0530

    added routes

--- routes/transaction.js
+++ routes/transaction.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { addTransaction, editTransaction, deleteTransaction, getAllTransactionsOfAccountPaginated,  } = require("../controllers/transaction");
+const { addTransaction, editTransaction, deleteTransaction, getAllTransactionsOfAccountPaginated, getBalanceTrend, getAccountSummary, getFilteredTransactionsPaginated,  } = require("../controllers/transaction");
 const authenticateUser = require("../middlewares/auth");
 
 const router = express.Router();
@@ -7,5 +7,8 @@ const router = express.Router();
 router.post("/create", authenticateUser, addTransaction);
 router.put("/edit/:transactionId",authenticateUser, editTransaction);
 router.delete("/delete/:transactionId",authenticateUser, deleteTransaction);
-router.get("/all/:accountId", authenticateUser,getAllTransactionsOfAccountPaginated)
+router.get("/all/:accountId", authenticateUser, getAllTransactionsOfAccountPaginated)
+router.get("/balance-trend/:accountId", authenticateUser, getBalanceTrend)
+router.get("/account-summary/:accountId", authenticateUser, getAccountSummary)
+router.get("/filter-transactions", authenticateUser, getFilteredTransactionsPaginated)
 module.exports = router;
